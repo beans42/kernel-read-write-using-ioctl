556a3f8742bbf8089bbbe377cc4b1c5c46cf71ec
beans42 <53321702+beans42@users.noreply.github.com>
2020-05-18 17:54:05 -0400

    Update main.cpp

--- usermode example/main.cpp
+++ usermode example/main.cpp
@@ -6,16 +6,16 @@ constexpr uintptr_t dwForceJump = 0x51ED760;
 constexpr uintptr_t m_fFlags = 0x104;
 constexpr uintptr_t FL_ONGROUND = 1 << 0;
 
-uintptr_t GetModule(const char* modName, DWORD procId) {
-	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+auto get_module(const char* module_name, DWORD process_id) {
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, process_id);
 	if (hSnap != INVALID_HANDLE_VALUE) {
 		MODULEENTRY32 modEntry;
 		modEntry.dwSize = sizeof(modEntry);
 		if (Module32First(hSnap, &modEntry)) {
 			do {
-				if (!strcmp(modEntry.szModule, modName)) {
+				if (!strcmp(modEntry.szModule, module_name)) {
 					CloseHandle(hSnap);
-					return (uintptr_t)modEntry.modBaseAddr;
+					return modEntry;
 				}
 			} while (Module32Next(hSnap, &modEntry));
 		}
@@ -25,17 +25,18 @@ uintptr_t GetModule(const char* modName, DWORD procId) {
 int main() {
 	auto hwnd = FindWindowA(NULL, "Counter-Strike: Global Offensive");
 	DWORD process_id; GetWindowThreadProcessId(hwnd, &process_id);
-	auto client = GetModule("client_panorama.dll", process_id);
+	auto client = get_module("client_panorama.dll", process_id);
+	auto client_base = (uintptr_t)client.modBaseAddr;
 
 	auto driver = new driver_manager("\\\\.\\cartidriver", process_id);
 
 	while (true) {
-		auto local_player = driver->RPM<uintptr_t>(client + dwLocalPlayer);
+		auto local_player = driver->RPM<uintptr_t>(client_base + dwLocalPlayer);
 		if (!local_player) continue;
 		auto flags = driver->RPM<DWORD>(local_player + m_fFlags);
 		if (flags & FL_ONGROUND)
 			if (GetAsyncKeyState(VK_SPACE) >> 15)
-				driver->WPM<DWORD>(client + dwForceJump, 6);
+				driver->WPM<DWORD>(client_base + dwForceJump, 6);
 		Sleep(5);
 	}
 }
